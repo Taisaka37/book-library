2d5fcda3c6da2bcf3dd1a86c6dc352124ff3c09e
Taisaka37 <125468545+Taisaka37@users.noreply.github.com>
2023-08-17 16:06:44 +0200

    update

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  myLibrary.push(atomicHabits);
 }
 
-document.querySelector("#button").onclick = function () {
-   
-}
+document.querySelector("#button").onclick = addBookToLibrary
+
+const atomicHabits = new Book('Atomic Habits', 'James Clear', '257 pages', 'Reading it')
